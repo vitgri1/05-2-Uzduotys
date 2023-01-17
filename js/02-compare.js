dccd20b5be4f590012bbd40ec593f5a50783e5a5
vitgri1 <vitgri1@gmail.com>
2023-01-17 14:00:29 +0200

    Compare

---
+++ js/02-compare.js
@@ -0,0 +1,158 @@
+console.clear();
+
+//1
+
+const mazasSkaicius = 2;
+const didelisSkaicius = 98;
+
+// - a
+if (mazasSkaicius > didelisSkaicius) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - b
+if (mazasSkaicius < didelisSkaicius) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - c
+if (mazasSkaicius === didelisSkaicius) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - d
+if (mazasSkaicius !== didelisSkaicius) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - e
+if (mazasSkaicius >= didelisSkaicius) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - f
+if (mazasSkaicius <= didelisSkaicius) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+//2
+
+const trumpasTekstas = 'geras';
+const ilgasTekstas = 'labai negeras pavyzdys';
+
+const trumpas = trumpasTekstas.length;
+const ilgas = ilgasTekstas.length;
+
+console.log(trumpas);
+console.log(ilgas);
+
+//3
+
+// - a
+if (trumpas > ilgas) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - b
+if (trumpas < ilgas) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - c
+if (trumpas === ilgas) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - d
+if (trumpas !== ilgas) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - e
+if (trumpas >= ilgas) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - f
+if (trumpas <= ilgas) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+//4
+
+const skaiciuSarasas = [5, 56, 51, 48, 1, 0, 9, -8, 5.6, 581];
+const zodziuSarasas = ['keptuve', 'puodas', 'lekste', 'puodelis', 'ekranas', 'spintele', 'stalas'];
+
+const kiekSkaiciu = skaiciuSarasas.length;
+const kiekZodziu = zodziuSarasas.length;
+
+console.log(kiekSkaiciu);
+console.log(kiekZodziu);
+
+//5
+
+// - a
+if (kiekSkaiciu > kiekZodziu) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - b
+if (kiekSkaiciu < kiekZodziu) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - c
+if (kiekSkaiciu === kiekZodziu) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - d
+if (kiekSkaiciu !== kiekZodziu) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - e
+if (kiekSkaiciu >= kiekZodziu) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
+
+// - f
+if (kiekSkaiciu <= kiekZodziu) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite kita karta');
+}
